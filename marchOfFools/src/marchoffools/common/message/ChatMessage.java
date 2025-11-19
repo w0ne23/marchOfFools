daac94072b842c9d24c286121701b191e72189c6
Jeong-dawon <dy030819@naver.com>
2025-11-19 15:15:10 +0900

    양방향 메시지

--- marchOfFools/src/marchoffools/common/message/ChatMessage.java
+++ marchOfFools/src/marchoffools/common/message/ChatMessage.java
@@ -5,8 +5,9 @@ import marchoffools.common.protocol.Message;
 public class ChatMessage extends Message {
     private static final long serialVersionUID = 1L;
     
-    private String content;
-    private String senderName;
+    private String senderName;    // 발신자 이름
+    private String content;       // 메시지 내용
+    private String roomId;        // 방 ID
     
     public ChatMessage() {
         super();
@@ -18,6 +19,22 @@ public class ChatMessage extends Message {
         this.content = content;
     }
     
+    public ChatMessage(String playerId, String senderName, String content, String roomId) {
+        super(playerId);
+        this.senderName = senderName;
+        this.content = content;
+        this.roomId = roomId;
+    }
+    
+    // Getters and Setters
+    public String getSenderName() {
+        return senderName;
+    }
+    
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+    
     public String getContent() {
         return content;
     }
@@ -26,11 +43,18 @@ public class ChatMessage extends Message {
         this.content = content;
     }
     
-    public String getSenderName() {
-        return senderName;
+    public String getRoomId() {
+        return roomId;
     }
     
-    public void setSenderName(String senderName) {
-        this.senderName = senderName;
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+    
+    @Override
+    public String toString() {
+        return "ChatMessage{sender=" + senderName + 
+               ", content='" + content + "'" +
+               ", roomId=" + roomId + "}";
     }
 }
--- marchOfFools/src/marchoffools/common/message/ConnectMessage.java
+++ marchOfFools/src/marchoffools/common/message/ConnectMessage.java
@@ -6,7 +6,6 @@ public class ConnectMessage extends Message {
     private static final long serialVersionUID = 1L;
     
     private String playerName;
-    private String version;
     
     public ConnectMessage() {
         super();
@@ -15,7 +14,6 @@ public class ConnectMessage extends Message {
     public ConnectMessage(String playerId, String playerName) {
         super(playerId);
         this.playerName = playerName;
-        this.version = "1.0";
     }
     
     public String getPlayerName() {
@@ -24,13 +22,5 @@ public class ConnectMessage extends Message {
     
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
-    }
-    
-    public String getVersion() {
-        return version;
-    }
-    
-    public void setVersion(String version) {
-        this.version = version;
     }
 }
